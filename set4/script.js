d67a89a2e89d2ae11f30f4c678a7108b4717e9cb
Nehaa7255 <nehamas04@gmail.com>
2024-06-20 16:09:17 +0530

    set 4 of ES6 created.

---
+++ set4/script.js
@@ -0,0 +1,12 @@
+/*
+1. Write an ES6 function that accepts an array of integers and returns
+ the maximum element in the array. Avoid using in-built methods.
+ / Your ES6 code here
+let array = [4, 78, 8, 3, 6, 0, 12, 34]
+console.log(getMaxElement(array)) // 78
+*/
+const getMaxElement=(arr)=>{
+    let array = [4, 78, 8, 3, 6, 0, 12, 34]
+    return arr.getMaxElement;
+};
+console.log(getMaxElement(array)) 
