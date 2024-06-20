a0dc8614b2e17f3395224a75ac753f6c68aab4ad
Nehaa7255 <nehamas04@gmail.com>
2024-06-20 16:42:25 +0530

    Question 2 completed.

--- set4/script.js
+++ set4/script.js
@@ -5,8 +5,37 @@
 let array = [4, 78, 8, 3, 6, 0, 12, 34]
 console.log(getMaxElement(array)) // 78
 */
-const getMaxElement=(arr)=>{
-    let array = [4, 78, 8, 3, 6, 0, 12, 34]
-    return arr.getMaxElement;
+const getMaxElement=(array)=>{
+let maxElement=array[0];
+
+for(let i=1;i<array.length;i++){
+    if(array[i]>maxElement){
+        maxElement=array[i];
+    }
+}
+return maxElement;
 };
-console.log(getMaxElement(array)) 
+let array = [4, 78, 8, 3, 6, 0, 12, 34]
+console.log(getMaxElement(array));
+
+/*
+2.  Write an ES6 function that takes an array of numbers and returns 
+the average of all the numbers. Avoid using in-built methods.
+/ Your ES6 code here
+console.log(calculateAverage([1, 2, 3, 4, 5])) // 3
+*/
+const calculateAverage=(arr)=>{
+    let sum=0;
+        for(let i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        let average=sum/arr.length;
+        return average;
+}
+console.log(calculateAverage([1, 2, 3, 4, 5]));
+
+/*
+3.  write an ES6 function that takes an array of numbers and converts
+    even numbers to odd numbers by adding 1 to that number.
+    */
+   
