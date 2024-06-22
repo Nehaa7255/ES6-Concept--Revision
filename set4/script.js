c99ed44af6f14ffc2d26ea2227203b6ffe37cb73
Nehaa7255 <nehamas04@gmail.com>
2024-06-22 11:58:04 +0530

    question 2 completed.

--- set4/script.js
+++ set4/script.js
@@ -37,5 +37,30 @@ console.log(calculateAverage([1, 2, 3, 4, 5]));
 /*
 3.  write an ES6 function that takes an array of numbers and converts
     even numbers to odd numbers by adding 1 to that number.
-    */
-   
+      / Your ES6 code here
+    var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    console.log(convertEvenToOdd(numArr))
+    / [1, 3, 3, 5, 5, 7, 7, 9, 9]*/
+
+
+
+
+
+    /*
+    4. write an ES6 function that takes an array of words and returns an array with all its elements whose length is greater than 5.
+  / Your ES6 code here
+var words = ['eat', 'sleep', 'code', 'repeat', 'neog', 'community']
+console.log(filterWords(words)) // ["repeat", "community"]
+*/
+
+
+
+
+
+/* 
+5.  Write an ES6 function that takes an array of strings and returns a new array with each string capitalized.
+/ Your ES6 code here
+console.log(capitalizeWords(['eat', 'sleep', 'code', 'repeat']))
+/ ["EAT", "SLEEP", "CODE", "REPEAT"]
+*/
+
