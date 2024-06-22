a29f9e65d6c195f1c5a5c3f64de077f44406000c
Nehaa7255 <nehamas04@gmail.com>
2024-06-22 22:49:44 +0530

    set 4 completed & ES6 completed.

--- set4/script.js
+++ set4/script.js
@@ -120,7 +120,7 @@ const podAndTeamAllocation=(arr)=>{
     return `{firstName: '${arr.firstName}', lastName: '${arr.lastName}', teamId: ${teamId}}`;
 }
 const userData = { firstName: 'John', lastName: 'Dee' }
-console.log(podAndTeamAllocation(userData));
+//console.log(podAndTeamAllocation(userData));
 
 /*
 8. Write an ES6 function which checks if a student already has a team.
@@ -163,9 +163,23 @@ console.log(author1) // David Flanagan
 console.log(author2) // Yukihiro Matsumoto
 console.log(publisherName) // O'Reilly Media
 */
+const book={
+title: 'JavaScript: The Definitive Guide',
+ authors: [
+ { name: 'David Flanagan', age: 49 },
+ { name: 'Yukihiro Matsumoto', age: 57 },
+ ],
+ publisher: { name: "O'Reilly Media", location: 'CA' },
+}
 
+const {title}= book;
+const { authors: [{ name: author1 }, { name: author2 }] } = book;
+const { publisher: { name: publisherName } } = book;
 
-
+//console.log(title) // JavaScript: The Definitive Guide
+//console.log(author1) // David Flanagan
+//console.log(author2) // Yukihiro Matsumoto
+//console.log(publisherName) // O'Reilly Media
 
 
 /*
@@ -192,3 +206,29 @@ age: 60,
 ]
 console.log(sumOfAges(array)) // 121
 */
+// Your ES6 code here
+const sumOfAges = (arr) => {
+    return arr.reduce((total, person) => total + person.age, 0);
+  }
+  
+  var arrays = [
+    {
+      name: 'Jay',
+      age: 60,
+    },
+    {
+      name: 'Gloria',
+      age: 36,
+    },
+    {
+      name: 'Manny',
+      age: 16,
+    },
+    {
+      name: 'Joe',
+      age: 9,
+    },
+  ]
+
+  console.log(sumOfAges(arrays));
+  
