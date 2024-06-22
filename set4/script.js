4618864bc1ab4f4fc9ef054473c66c38b44895ed
Nehaa7255 <nehamas04@gmail.com>
2024-06-22 16:42:24 +0530

    Question 10 completed.

--- set4/script.js
+++ set4/script.js
@@ -120,7 +120,7 @@ const podAndTeamAllocation=(arr)=>{
     return `{firstName: '${arr.firstName}', lastName: '${arr.lastName}', teamId: ${teamId}}`;
 }
 const userData = { firstName: 'John', lastName: 'Dee' }
-console.log(podAndTeamAllocation(userData))
+console.log(podAndTeamAllocation(userData));
 
 /*
 8. Write an ES6 function which checks if a student already has a team.
@@ -135,9 +135,20 @@ console.log(checkForTeam({ firstName: 'Priya', lastName: 'Raj' }))
 / {firstName: 'Priya', lastName: 'Raj', team: A}
 */
 
-
-
-
+function checkForTeam(student) {
+    if(!student.team){
+        student.team='A';
+    }
+    return student;
+}
+  
+  console.log(checkForTeam({ firstName: 'Penn', lastName: 'Ma' }));
+  
+  console.log(checkForTeam({ firstName: 'John', lastName: 'Dee', team: 'B' }));
+  
+  console.log(checkForTeam({ firstName: 'Priya', lastName: 'Raj' }));
+  
+  
 /*
 9. Destructure the following code to get the desired outputs. Avoid 
    using in-built methods.
@@ -155,6 +166,8 @@ console.log(author1) // David Flanagan
 console.log(author2) // Yukihiro Matsumoto
 console.log(publisherName) // O'Reilly Media
 */
+
+
 
 
 /*
